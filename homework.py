3b7d8cb96dd8c7ac34f778d654643956cdc569b9
Сергей Шевченко <single1709@ya.ru>
2022-03-20 17:17:42 +0300

    44

--- homework.py
+++ homework.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
